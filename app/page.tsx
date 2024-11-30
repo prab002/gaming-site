1acd8c1a9f6b25613c73982144fe7e0e75c26ad8
prab002 <prabhanjan.sharma@invennico.com>
2024-11-30 13:10:41 +0530

    added the hero Desing

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidden ">
       <Hero />
+      <section className=" min-h-screen"></section>
     </div>
   );
 }
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,8 +1,13 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../common/Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -10,7 +15,7 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [loadedVideo, setLoadedVideo] = useState(0);
 
-  const totalVideo = 3;
+  const totalVideo = 4;
   const nextVdRef = useRef(null);
 
   const upcomingVideo = (currentIndex % totalVideo) + 1;
@@ -27,10 +32,70 @@ const Hero = () => {
 
   let getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
+  useEffect(() => {
+    if (loadedVideo === totalVideo - 1) {
+      setIsLoading(false);
+    }
+  }, [loadedVideo]);
+  //@Gsap animation
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.to("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVdRef.current.play(),
+        });
+      }
+      gsap.from("current-video", {
+        transformOrigin: "center center",
+        scale: 0,
+        duration: 1.5,
+        ease: "power1.inOut",
+      });
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP(() => {}, [
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: " 0 0 10% 10%",
+    }),
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center ",
+        end: "bottom center",
+        scrub: true,
+      },
+    }),
+  ]);
+
   return (
     <section className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading === false && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
-        id="video-frame "
+        id="video-frame"
         className="relative  z-10  h-dvh w-screen overflow-hidden rounded-lg bg-blue-75  "
       >
         <div className="">
@@ -48,7 +113,7 @@ const Hero = () => {
                 onLoadedData={() => {
                   handleVideoLoad();
                 }}
-                src={getVideoSrc(upcomingVideo)}
+                src={getVideoSrc((currentIndex % totalVideo) + 1)}
               />
             </div>
           </div>
@@ -62,24 +127,27 @@ const Hero = () => {
           />
           <video
             src={getVideoSrc(
-              currentIndex === totalVideo - 1 ? 1 : currentIndex + 1
+              currentIndex === totalVideo - 1 ? 1 : currentIndex
             )}
             className="absolute left-0 top-0 size-full object-cover object-center "
             autoPlay
             loop
             muted
+            onLoadedData={handleVideoLoad}
           />
         </div>
-        <h1 className=" absolute font-zentry text-7xl font-black bottom-5 right-5 z-50 text-blue-75">
+        <h1 className=" absolute font-zentry text-7xl font-black bottom-10 right-10 z-50 text-blue-75">
           PRAB
         </h1>
         <div className="left-0 top-0 absolute z-40 size-full ">
           <div className="mt-24 p-5 sm:p-10 `">
             <h1 className="text-6xl font-black font-zentry  text-blue-75">
-              REDFIN<b>N</b>E
+              DEVELOPER
             </h1>
             <p className="mt-1  max-w-64 font-robert-regular text-blue-100">
-              Enter the metagame <br /> Desing by Prab
+              Working on Gsap And Next js
+              <br />
+              There is more on git
             </p>
             <Button
               id="watch-trailer"
@@ -89,10 +157,10 @@ const Hero = () => {
             />
           </div>
         </div>
-        <h1 className=" absolute font-zentry text-7xl font-black bottom-5 right-5  text-black">
-          PRAB
-        </h1>
       </div>
+      <h1 className=" absolute font-zentry text-7xl font-black bottom-10 right-10 text-black">
+        PRAB
+      </h1>
     </section>
   );
 };
